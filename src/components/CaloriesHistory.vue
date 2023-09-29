0f96941814bc17df2b28e801f4026aa9d379806f
grzejatol <grzejatol@gmail.com>
2023-09-29 19:27:48 +0100

    dodana historia zaczynam standard items

---
+++ src/components/CaloriesHistory.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+
+defineProps({
+     calHistory: {
+      type: Number
+      },
+      id: {
+      type: Number
+      }
+});
+
+function deleteHistoryItem(item: any, id: any) {
+  console.log(item);
+  console.log(id);
+
+}
+</script>
+
+<template>
+<div class="historyCalories" >
+  {{calHistory}}
+  <p @click="$emit('deleteHistoryItem', calHistory, id)">X</p>
+</div>
+</template>
+
+<style scoped>
+.historyCalories {
+  position: relative;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+  background-color: #a7b9fd;
+  border: 1px solid gray;
+  padding: 5px;
+  border-top-right-radius: 20px 25px;
+  border-bottom-right-radius: 3px;
+  border-bottom-left-radius: 5px;
+  border-top-left-radius: 3px;
+  margin-left: 10px;
+  margin-top: 2px;
+}
+p {
+  left: 100%;
+  transform: translateX(-50%);
+  top: -10%;
+  position: absolute;
+  margin: 0;
+  padding: 0;
+
+  cursor: pointer;
+}
+p:hover {
+  color: red;
+}
+</style>
--- src/components/MyCalories.vue
+++ src/components/MyCalories.vue
@@ -10,7 +10,14 @@ Liczba kalori full {{name}}
    <button class="addButton" type="submit">Add</button>
 
   </form>
-  {{calories}}
+ <h2>{{totalCalories}}</h2>
+  <div class="historyFull">
+  <CaloriesHistory v-for="calorie in caloriesHistory"
+                   :calHistory="calorie" :id="caloriesHistory.indexOf(calorie)"
+                   :key="caloriesHistory.indexOf(calorie)"
+                    @deleteHistoryItem="deleteHistoryItem"
+  />
+  </div>
   <CaloriesAlert v-if="wrongInput" :caloriesAlert="wrongInput" @zamknij="wrongInput = ''"/>
 </template>
 
@@ -18,19 +25,22 @@ Liczba kalori full {{name}}
 <script setup lang="ts">
 import {ref, computed} from "vue";
 import CaloriesAlert from "@/components/CaloriesAlert.vue";
+import CaloriesHistory from "@/components/CaloriesHistory.vue";
 
 defineProps({
 
   name: String,
 })
-let calories = ref(0);
+let caloriesHistory = ref([]);
+let totalCalories = ref(0);
 let nowaLiczbaCalori = ref('');
 let wrongInput = ref('');
 function newCalories() {
   if (nowaLiczbaCalori.value === '') {
     return;
   }
-  calories.value = nowaLiczbaCalori.value;
+  caloriesHistory.value.push(nowaLiczbaCalori.value);
+  totalCalories.value = parseInt(nowaLiczbaCalori.value)  + parseInt(totalCalories.value);
   nowaLiczbaCalori.value = '';
   wrongInput.value = '';
 }
@@ -44,13 +54,56 @@ function validateNumericInput() {
     wrongInput.value = testValue ;
   }
 }
+function deleteHistoryItem(item: any, id: any) {
+  console.log(item);
+  console.log(id);
+  caloriesHistory.value.splice(id, 1);
+  totalCalories.value = parseInt(totalCalories.value) - parseInt(item);
+}
 </script>
 
 
 
 <style scoped>
 .caloriesFull {
-  background-color: antiquewhite;
+padding: 10px 0;
+  background-color: #d1d2d3;
   margin: 5px;
+  border: 1px solid gray;
+  box-shadow: rgba(157, 159, 166, 0.8) 2px 5px 2px 0;
+}
+.form {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+.addButton {
+  margin: 5px;
+  padding: 5px;
+  border-radius: 4px;
+  border: 1px solid gray;
+  background-color: rgb(255,255,255);
+  font-size: 1.1rem;
+  box-shadow: rgba(128,128,128, 0.3) 1px 3px 2px 1px ;
+}
+.addButton:hover {
+  background-color: gray;
+  color: white;
+}
+input {
+  padding: 0.2rem;
+  border-radius: 4px;
+  box-shadow: rgba(128,128,128, 0.3) 1px 3px 2px 1px ;
+  list-style: none;
+  border: 1px solid gray;
+  font-size: 1.1rem;
+}
+.historyFull {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+
+
 }
 </style>
